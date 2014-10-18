70ca981c46a3b4a9f0a8676aa4ab5e5093152659
Marko Ivanek <markoivanek@gmail.com>
2014-10-18 09:34:35 +0200

    Enable hstore

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018063604) do
+ActiveRecord::Schema.define(version: 20141018071684) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -34,4 +35,5 @@ ActiveRecord::Schema.define(version: 20141018063604) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
 end
