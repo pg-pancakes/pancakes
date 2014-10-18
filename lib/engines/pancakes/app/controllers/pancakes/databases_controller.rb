8de72b2fb65d9a082ef283bc0765d14aae28fb2a
Damir Svrtan <damir.svrtan@gmail.com>
2014-10-18 09:04:34 +0200

    DatabaseController#index redirect to default show.

--- lib/engines/pancakes/app/controllers/pancakes/databases_controller.rb
+++ lib/engines/pancakes/app/controllers/pancakes/databases_controller.rb
@@ -1,7 +1,11 @@
 module Pancakes
   class DatabasesController < ApplicationController
     def show
+      # connect to pg/database/:database
+    end
 
+    def index
+      redirect_to database_path(Rails.env)
     end
   end
 end
