626a100650ee0f3aeaf0bf5aa1e670f9d09d2627
Damir Svrtan <damir.svrtan@gmail.com>
2014-10-19 21:46:59 +0200

    On production only show production database.

--- lib/engines/pancakes/lib/pancakes/engine.rb
+++ lib/engines/pancakes/lib/pancakes/engine.rb
@@ -5,7 +5,9 @@ module Pancakes
     initializer "pancakes.connection" do
       Pancakes.database_yml_path ||= Rails.root.join('config', 'database.yml')
       Pancakes.pancakes_yml_path ||= Rails.root.join('config', 'pancakes.yml')
-      Pancakes.environments = Dir[Rails.root.join('config', 'environments', '*.rb')].map { |filename| File.basename(filename, ".rb") }
+      Pancakes.environments ||= begin
+        Dir[Rails.root.join('config', 'environments', '*.rb')].map { |filename| File.basename(filename, ".rb") }
+      end
     end
   end
 end
